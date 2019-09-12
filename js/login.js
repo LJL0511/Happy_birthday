600e2f151a27c9c46446d77b42a109e172910e2b
LJL0511 <Lijl0511@163.com>
2019-09-12 14:28:00 +0800

    zyhbirthday（8-14）

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="zr" &&  pwd=="0701"){
+    if(userName=="zyh" &&  pwd=="0814"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
